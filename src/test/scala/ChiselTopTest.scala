7100b8e938376f546b86e6157fac136ecf59bb85
Martin Schoeberl <martin@jopdesign.com>
2024-04-15 21:47:18 +0200

    Try a single tile

--- src/test/scala/ChiselTopTest.scala
+++ src/test/scala/ChiselTopTest.scala
@@ -4,10 +4,11 @@ import org.scalatest.flatspec.AnyFlatSpec
 class ChiselTopTest extends AnyFlatSpec with ChiselScalatestTester {
 
   "ChiselTop" should "pass" in {
-    test(new ChiselTop()).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+    test(new ChiselTop()).withAnnotations(Seq(WriteVcdAnnotation, VerilatorBackendAnnotation)) { dut =>
       dut.io.ui_in.poke(1.U)
       dut.io.uio_in.poke(2.U)
       dut.io.ena.poke(true.B)
+      dut.clock.setTimeout(0)
       dut.clock.step(1000)
     }
   }
