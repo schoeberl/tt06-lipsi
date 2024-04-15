db64f52042f7dd17bfb2e3bd9a9470efbaf8286c
Martin Schoeberl <martin@jopdesign.com>
2024-04-15 22:11:38 +0200

    Use a 7-segment decoder for the counting

--- src/main/scala/ChiselTop.scala
+++ src/main/scala/ChiselTop.scala
@@ -1,7 +1,47 @@
 import chisel3._
+import chisel3.util._
 
 import lipsi._
 
+class Decoder extends Module {
+
+  val counter = IO(Input(UInt(4.W)))
+  val segments = IO(Output(UInt(7.W)))
+
+  segments := 0.U
+  switch (counter) {
+    is(0.U) {
+      segments := "b0111111".U
+    }
+    is(1.U) {
+      segments := "b0000110".U
+    }
+    is(2.U) {
+      segments := "b1011011".U
+    }
+    is(3.U) {
+      segments := "b1001111".U
+    }
+    is(4.U) {
+      segments := "b1100110".U
+    }
+    is(5.U) {
+      segments := "b1101101".U
+    }
+    is(6.U) {
+      segments := "b1111100".U
+    }
+    is(7.U) {
+      segments := "b0000111".U
+    }
+    is(8.U) {
+      segments := "b1111111".U
+    }
+    is(9.U) {
+      segments := "b1100111".U
+    }
+  }
+}
 /**
  * Example design in Chisel.
  * A redesign of the Tiny Tapeout example.
@@ -23,7 +63,7 @@ class ChiselTop() extends Module {
   val add = WireDefault(0.U(7.W))
   add := io.ui_in + io.uio_in
 
-  val lipsi = Module(new LipsiTop("lipsi/asm/test.asm"))
+  val lipsi = Module(new Lipsi("lipsi/asm/blink.asm"))
   lipsi.io.din := add
 
   // Blink with 1 Hzq
@@ -34,7 +74,13 @@ class ChiselTop() extends Module {
     cntReg := 0.U
     ledReg := ~ledReg
   }
-  io.uo_out := ledReg ## lipsi.io.dout(6, 0)
+  val dec = Module(new Decoder)
+  dec.counter := lipsi.io.dout(3, 0)
+  val seg = dec.segments
+  val led = Mux(io.ui_in(0), ledReg, 0.U)
+
+  io.uo_out := led ## seg
+  // io.uo_out := ledReg ## lipsi.io.dout(6, 0)
 }
 
 object ChiselTop extends App {
--- src/test/scala/ChiselTopTest.scala
+++ src/test/scala/ChiselTopTest.scala
@@ -4,7 +4,8 @@ import org.scalatest.flatspec.AnyFlatSpec
 class ChiselTopTest extends AnyFlatSpec with ChiselScalatestTester {
 
   "ChiselTop" should "pass" in {
-    test(new ChiselTop()).withAnnotations(Seq(WriteVcdAnnotation, VerilatorBackendAnnotation)) { dut =>
+    // test(new ChiselTop()).withAnnotations(Seq(WriteVcdAnnotation, VerilatorBackendAnnotation)) { dut =>
+    test(new ChiselTop()).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       dut.io.ui_in.poke(1.U)
       dut.io.uio_in.poke(2.U)
       dut.io.ena.poke(true.B)
